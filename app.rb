f0fcdf43aa3de1c2da49281ea8a35c97ab7f06b4
paul-tes <pauasca8462@gmail.com>
2023-07-28 11:18:44 +0300

    Best way app menue loop

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 class App
-  def menue
+  def display_menu
     puts 'Welcome to School library App!'
     puts
-    puts 'please choose an option by enterin a number: '
+    puts 'Please choose an option by entering a number: '
     puts '1 - List all books'
     puts '2 - List all people'
     puts '3 - Create a person'
@@ -20,7 +20,7 @@ class App
     'peoples'
   end
 
-  def create_preson
+  def create_person
     'teacher or student'
   end
 
@@ -32,15 +32,35 @@ class App
     'create rental'
   end
 
-  def get_rental(id)
-    'retals with id'
+  def get_rental_for_person(id)
+    "rentals with id #{id}"
+  end
+
+  def exit_app?
+    puts 'Exiting app ....'
+    @exit = true
   end
 
   def run
-    loop do
-      puts menue
-      order = gets.chomp
-      break if order == '7'
+    @exit = false
+    until @exit
+      display_menu
+      choice = gets.chomp
+      menu_options[choice]&.call || puts('Invalid input. Please try again')
     end
+  end
+
+  private
+
+  def menu_options
+    {
+      '1' => method(:all_books),
+      '2' => method(:all_peoples),
+      '3' => method(:create_person),
+      '4' => method(:create_book),
+      '5' => method(:create_rental),
+      '6' => -> { get_rental_for_person(gets.chomp) },
+      '7' => method(:exit_app?)
+    }
   end
 end
