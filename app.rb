cc35ea572460951a3084eef1df5206921a765a75
paul-tes <pauasca8462@gmail.com>
2023-07-28 09:32:01 +0300

    Basic structure

---
+++ app.rb
@@ -0,0 +1,34 @@
+class App
+  def menue
+    'menue'
+  end
+
+  def all_books
+    'books'
+  end
+
+  def all_peoples
+    'peoples'
+  end
+
+  def create_preson
+    'teacher or student'
+  end
+
+  def create_book
+    'create books'
+  end
+
+  def create_rental
+    'create rental'
+  end
+
+  def get_rental(id)
+    'retals with id'
+  end
+
+  def run
+    puts 'Welcome to our library'
+    puts menue
+  end
+end
