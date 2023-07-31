93df3974a2175a0cc02900ada2daca642420d39d
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-07-31 14:17:24 +0000

    Modify main.rb file to run and display menu to user

--- main.rb
+++ main.rb
@@ -1,7 +1,16 @@
 # This is a console app School Library that can add new students or teachers,
 # add new books, and save records of who borrowed a given book and when. built with Ruby.
-
+​
 require_relative 'app'
-
-shcool_library = App.new
-shcool_library.run
+​
+def main
+  app = App.new
+  app.exit = false
+  until app.exit
+    app.display_menu
+    choice = gets.chomp
+    app.menu_options[choice]&.call
+  end
+end
+​
+main
