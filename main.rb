e90a2b1092ca43f7398d8f3344f373ae2b70a3be
paul-tes <pauasca8462@gmail.com>
2023-07-31 17:31:06 +0300

    Fix rubocop in app.rb and main.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 # This is a console app School Library that can add new students or teachers,
 # add new books, and save records of who borrowed a given book and when. built with Ruby.
-​
+
 require_relative 'app'
-​
+
 def main
   app = App.new
   app.exit = false
@@ -12,5 +12,5 @@ def main
     app.menu_options[choice]&.call
   end
 end
-​
+
 main
