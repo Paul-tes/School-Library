ee0be4fecf6daec9f0c5d180bd9d2ec7be5031d0
paul-tes <pauasca8462@gmail.com>
2023-07-31 17:23:34 +0300

    Add store class

---
+++ store.rb
@@ -0,0 +1,24 @@
+class Store
+  attr_accessor :rentals, :person, :books
+
+  def initialize
+    @rentals = []
+    @person = []
+    @books = []
+  end
+
+  def add_person(person)
+    @person << person
+    puts "#{person.name} created Successfully"
+  end
+
+  def add_book(book)
+    @books << book
+    puts "#{book.title} Book created successfully"
+  end
+
+  def add_rental(rental)
+    @rentals << rental
+    puts 'Rental created successfully'
+  end
+end
